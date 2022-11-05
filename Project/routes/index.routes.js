37beee22ecabf6867d290a813ce4b70b29a9de7a
Yaron Peres <yaronp16@gmail.com>
2022-11-05 12:58:12 +0100

    routed check

--- Project/routes/index.routes.js
+++ Project/routes/index.routes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
+//const { default: mongoose } = require('mongoose');
+const Comic = require("../models/Comics.model")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 // ------------------------ catalogue Routes ------------------------
-router.get("/catalogue", (req, res, next) => {
-  res.render("catalogue")
+router.get("/catalogue", async (req, res, next) => {
+  console.log("We are inside the catalogue!")
+  try{
+  const allComics = await Comic.find()
+  console.log("This are all the comics : ", {allComics})
+  res.render("catalogue", {allComics})
+  } catch (err) {
+    console.log("Error getting catalogue:" + err)
+  }
 })
 // --------------------- Product details Routes ------------------------
 router.get("/product-details", (req, res, next) => {
