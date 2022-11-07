fbe6f3c6c4af7df8c81d62d9c97abd9ce5d7cfa0
Giuliano Sacco <saccogiuliano99@gmail.com>
2022-11-07 13:06:22 +0100

    Description

--- Project/routes/index.routes.js
+++ Project/routes/index.routes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const Activity = require("../models/Activity.model")
+
+
 /* GET home page */
-router.get("/", (req, res, next) => {
-  const user = req.session.currentUser;
-  res.render("index", {user});
-});
+router.get("/", async (req, res, next) => {
+    try{
+      const activityDb = await Activity.find()
+       console.log(activityDb)
+       res.render("index", {activityDb});
+   }catch(err){
+       console.log(err)
+   }
+  });
+  
+
+
 
 module.exports = router;
