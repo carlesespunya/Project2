c18369964d248d2d3abcf422ef601244567c4955
Milky Raven <heartonllamas@gmail.com>
2022-11-07 15:58:28 +0100

    cart working

--- Project/models/Items.model.js
+++ Project/models/Items.model.js
@@ -1,10 +1,15 @@
 const { Schema, model } = require("mongoose");
+const Cart = require("./ShoppingCart.model");
 
 const itemSchema = new Schema(
   {
     comicId: {
         type: Schema.Types.ObjectId, 
         ref: 'Comic'
+    },
+    cartId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Cart'
     }
   }
 );
