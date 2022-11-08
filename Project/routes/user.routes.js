85dba8d2f3f83b1e4d084e1e8e366e1c6d025147
Vicky Ricart <vickyc.ricart@gmail.com>
2022-11-08 11:29:43 +0100

    userProfile-working on it

--- Project/routes/user.routes.js
+++ Project/routes/user.routes.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-// const User = require("../models/User.model")
+const User = require("../models/User.model")
 
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
+
+
 
 router.get("/user-profile", isLoggedIn, (req, res) => {
     const user = req.session.currentUser;
     res.render("user/user-profile", {user});
 })
+
 
 
 module.exports = router
